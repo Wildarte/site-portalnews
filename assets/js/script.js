9b8c2b9697720a77dda0e92a29397d7ca2f46138
wildarte <wildarte10@gmail.com>
2023-02-11 17:01:06 -0300

    second commit

---
+++ assets/js/script.js
@@ -0,0 +1,16 @@
+//=============== letter auto ===============
+const itens_letter = document.querySelectorAll('.top_content_list');
+
+let count = 0;
+itens_letter.forEach((item) => {
+
+    /*
+    setInterval(function(){
+        count += 1;
+        console.log(count);
+        item.style.left = "-"+count+"px";
+    },30);
+    */  
+
+});
+//=============== letter auto ===============
